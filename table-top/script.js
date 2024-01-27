dc2831d42ab8eaa10b316afe883aedcf46b755a3
Michael Gutensohn <michael@volumetrics.io>
2024-01-26 17:13:57 -0800

    tabletop finished.

--- table-top/script.js
+++ table-top/script.js
@@ -6,28 +6,37 @@ var __appState = {
         {
             name: "Koi fish",
             order: "Cypriniformes",
-            model: 'koifish.glb',
+            model: 'koi/koifish.glb',
+            volumeModel: 'koi/koifish_swim_circle.glb',
             scale: 0.05,
+            scale2: 0.025,
             rotation: "0 0 0",
             position: "0 -0.02 0",
+            volumePosition: "0 0.3 0",
             desc: "Koi fish are colored varieties of the common carp. Koi or more specifically nishikigoi are kept for decorative purposes in outdoor koi ponds or water gardens in Japan. Major colors of Koi fish are white, black, red, orange, yellow, blue, brown and cream."
         },
         {
             name: "Hammerhead Shark",
             order: "Carcharhiniformes",
-            model: 'great_hammerhead_shark.glb',
+            model: 'hammerhead/great_hammerhead_shark.glb',
+            volumeModel: 'hammerhead/hammerhead_circle_swim1.glb',
             scale: 0.15,
+            scale2: 0.06,
             rotation: "0 90 0",
             position: "0.05 0.07 0",
+            volumePosition: "0 0.3 0",
             desc: "The Great Hammerhead Shark is found in tropical and warm temperate waters worldwide, inhabiting coastal areas and the continental shelf. A solitary, strong-swimming apex predator, the great hammerhead feeds on a wide variety of prey ranging from crustaceans and cephalopods, to bony fish, to smaller sharks.",
         },
         {
             name: "Blue Whale",
             order: "Artiodactyla",
-            model: 'blue_whale.glb',
+            model: 'whales/blue_whale.glb',
+            volumeModel: 'whales/whale_circle_2.glb',
             scale: 0.0005,
+            scale2: 0.045,
             rotation: "0 90 360",
             position: "0 -0.05 0",
+            volumePosition: "0 0.415 0",
             desc: "The Blue Whale (Balaenoptera musculus) is a marine mammal and a baleen whale. It is the largest animal known ever to have existed. The blue whale's long and slender body can be of various shades of greyish-blue dorsally and somewhat lighter underneath."
         }
     ],
@@ -49,9 +58,19 @@ var __appState = {
         document.querySelector("#desc").innerText = selectedFish.desc;
 
         let childArray = Array.from(document.querySelector("#models").childNodes);
+        let aquariumModels = Array.from(document.querySelector("#aquarium-models").childNodes);
 
         // Hide all the fishes but the selected one
         childArray.forEach(child => {
+            if (child.dataset.name == App.fishes[App.selected].name) {
+                child.object3D.visible = true;
+
+            } else {
+                child.object3D.visible = false;
+            }
+        });
+
+        aquariumModels.forEach(child => {
             if (child.dataset.name == App.fishes[App.selected].name) {
                 child.object3D.visible = true;
 
@@ -107,12 +126,24 @@ App.fishes.forEach(fish => {
     model.dataset.name = fish.name;
     model.dataset.rotation = fish.rotation;
     model.dataset.position = fish.position;
+    
+    let volumeModel = document.createElement("mr-model");
+
+    volumeModel.setAttribute("src", "./assets/" + fish.volumeModel);
+    volumeModel.dataset.name = fish.name;
+    volumeModel.dataset.position = fish.volumePosition;
+    volumeModel.object3D.visible = false;
 
     Object.assign(model.style, {
         scale: fish.scale
     })
 
+    Object.assign(volumeModel.style, {
+        scale: fish.scale2
+    })
+
     document.querySelector("#models").append(model);
+    document.querySelector("#aquarium-models").append(volumeModel);
 });
 
 document.addEventListener('DOMContentLoaded', function () {
