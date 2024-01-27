35feabcc1edfb64f02cdb6f0fae5b9cfcecb78b2
Laurent Baumann <git@lobau.io>
2024-01-27 13:17:51 -0800

    demo improvements

--- 02-interactive/script.js
+++ 02-interactive/script.js
@@ -2,11 +2,11 @@ var __appState = {
     selected: 0,
     debugToggle: false,
     isLogoAnimating: false,
-    fishes: [
-        {
+    fishes: [{
             name: "Koi fish",
             order: "Cypriniformes",
             model: 'koifish.glb',
+            link: 'https://en.wikipedia.org/wiki/Koi',
             scale: 0.05,
             rotation: "0 0 0",
             position: "0 -0.02 0",
@@ -16,6 +16,7 @@ var __appState = {
             name: "Hammerhead Shark",
             order: "Carcharhiniformes",
             model: 'great_hammerhead_shark.glb',
+            link: 'https://en.wikipedia.org/wiki/Hammerhead_shark',
             scale: 0.15,
             rotation: "0 90 0",
             position: "0.05 0.07 0",
@@ -25,26 +26,29 @@ var __appState = {
             name: "Blue Whale",
             order: "Artiodactyla",
             model: 'blue_whale.glb',
+            link: 'https://en.wikipedia.org/wiki/Blue_whale',
             scale: 0.0005,
             rotation: "0 90 360",
             position: "0 -0.05 0",
             desc: "The Blue Whale (Balaenoptera musculus) is a marine mammal and a baleen whale. It is the largest animal known ever to have existed. The blue whale's long and slender body can be of various shades of greyish-blue dorsally and somewhat lighter underneath."
         }
     ],
-    next: function () {
+    next: function() {
         if (this.selected < this.fishes.length - 1) {
             this.selected++
         }
     },
-    prev: function () {
+    prev: function() {
         if (this.selected > 0) {
             this.selected--
         }
     },
-    render: function () {
+    render: function() {
         let selectedFish = this.fishes[this.selected];
 
         document.querySelector("#name").innerText = selectedFish.name;
+        document.querySelector("#link").innerText = `Read more`;
+        document.querySelector("#link").setAttribute("href", selectedFish.link);
         document.querySelector("#order").innerText = selectedFish.order;
         document.querySelector("#desc").innerText = selectedFish.desc;
 
@@ -60,9 +64,9 @@ var __appState = {
             }
         });
     },
-    spinLogo: function () {
+    spinLogo: function() {
 
-        if(this.isLogoAnimating) {
+        if (this.isLogoAnimating) {
             clearInterval(this.spinAnimaton);
             this.isLogoAnimating = false;
             document.querySelector("#mrjs-logo").dataset.rotation = "0 0 0";
@@ -74,18 +78,18 @@ var __appState = {
             }, 16)
             this.isLogoAnimating = true;
         }
+    },
+    toggleDebug: function() {
+        if (this.debugToggle) {
+            document.querySelector("mr-app").removeAttribute("debug");
+            this.debugToggle = false;
+        } else {
+            document.querySelector("mr-app").setAttribute("debug", true);
+            this.debugToggle = true;
+        }
+        console.log(document.querySelector("mr-app"));
+        console.log(this.debugToggle);
     }
-    // toggleDebug: function() {
-    //     if(this.debugToggle) {
-    //         document.querySelector("mr-app").removeAttribute("debug");
-    //         this.debugToggle = false;
-    //     } else {
-    //         document.querySelector("mr-app").setAttribute("debug", true);
-    //         this.debugToggle = true;
-    //     }
-    //     console.log(document.querySelector("mr-app"))
-    //     console.log(this.debugToggle)
-    // }
 }
 
 // Magic reactivity
@@ -115,6 +119,6 @@ App.fishes.forEach(fish => {
     document.querySelector("#models").append(model);
 });
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
     App.selected = 0;
 });
